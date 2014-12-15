60482db80459311f3b063fe48736790eac1bc9a9
Guest <guest-yJIRFF@meet.mit.edu>
2014-12-15 19:55:29 +0200

    hah bullshit

--- meet-django/pollingsite/polls/views.py
+++ meet-django/pollingsite/polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from models import Poll, answer
+from models import Polls, answer
 def vote(request):
-	return HttpResponse(' SaDeK J. Master Mind')
+	return render(request, 'history.html')
 # Create your views(functions) here.
 # Remember each function/view the first argument/input has to be request
